1db60a87152edbbef74d3dc913a80b3a76d0b2a6
星梦StarsDream <starsdream00@icloud.com>
2022-12-04 23:11:41 +0800

    修复

--- Yanzheng/Program.cs
+++ Yanzheng/Program.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using Telegram.Bot;
+using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 using Yanzheng.Utils;
+using File = System.IO.File;
+using Timer = System.Timers.Timer;
 
 Config config = new()
 {
@@ -24,7 +28,7 @@ if (!File.Exists("config.json"))
 }
 config = JsonSerializer.Deserialize<Config>(File.ReadAllText("config.json"));
 
-Dictionary<long, Dictionary<int, long>> data = new();
+Dictionary<long, Dictionary<long, int>> data = new();
 TelegramBotClient botClient = new(config.Token, string.IsNullOrWhiteSpace(config.Proxy)
     ? default
     : new(new HttpClientHandler
@@ -35,21 +39,74 @@ botClient.StartReceiving((_, update, _) =>
 {
     if (update.Type is UpdateType.CallbackQuery)
     {
-        _ = botClient.SendTextMessageAsync(update.CallbackQuery.From.Id, "已确认");
-        _ = botClient.ApproveChatJoinRequest(data[update.CallbackQuery.From.Id][update.CallbackQuery.Message.MessageId], update.CallbackQuery.From.Id);
-        data.Remove(update.CallbackQuery.From.Id);
+        if (
+            (update.CallbackQuery.Data is "1" && !botClient.GetChatAdministratorsAsync(update.CallbackQuery.Message.Chat.Id).Result.Any((chatMember) => chatMember.User.Id == update.CallbackQuery.From.Id))
+            || !data.ContainsKey(update.CallbackQuery.From.Id)
+            || !data[update.CallbackQuery.From.Id].ContainsKey(update.CallbackQuery.Message.Chat.Id)
+            || data[update.CallbackQuery.From.Id][update.CallbackQuery.Message.Chat.Id] != update.CallbackQuery.Message.MessageId
+           )
+        {
+            _ = botClient.AnswerCallbackQueryAsync(update.CallbackQuery.Id, "验证失败");
+            return;
+        }
+        _ = botClient.RestrictChatMemberAsync(update.CallbackQuery.Message.Chat.Id, update.CallbackQuery.From.Id, update.Message.Chat.Permissions, DateTime.UtcNow);
+        _ = botClient.AnswerCallbackQueryAsync(update.CallbackQuery.Id, "验证成功");
+        _ = botClient.DeleteMessageAsync(update.CallbackQuery.Message.Chat.Id, update.CallbackQuery.Message.MessageId);
+        _ = data[update.CallbackQuery.From.Id].Remove(update.CallbackQuery.Message.Chat.Id);
         return;
     }
-    if (update.Type is not UpdateType.ChatJoinRequest || !update.ChatJoinRequest.From.IsBot)
+    if (
+        !botClient.GetChatAdministratorsAsync(update.Message.Chat.Id).Result.Any((chatMember) => chatMember.User.Id == botClient.BotId)
+        || update.Type is not UpdateType.Message
+        || update.Message.Type is not MessageType.ChatMembersAdded
+       )
     {
         return;
     }
-    if (!data.ContainsKey(update.CallbackQuery.From.Id))
+    foreach (User member in update.Message.NewChatMembers)
     {
-        data.Add(update.ChatJoinRequest.From.Id, new());
+        if (member.IsBot)
+        {
+            continue;
+        }
+        if (!data.ContainsKey(member.Id))
+        {
+            data[member.Id] = new();
+        }
+        _ = botClient.RestrictChatMemberAsync(update.Message.Chat.Id, member.Id, new ChatPermissions()
+        {
+            CanSendMessages = false,
+            CanSendMediaMessages = false,
+            CanSendPolls = false,
+            CanSendOtherMessages = false,
+            CanAddWebPagePreviews = false,
+            CanChangeInfo = false,
+            CanInviteUsers = false,
+            CanPinMessages = false,
+            CanManageTopics = false
+        });
+        Message msg = botClient.SendTextMessageAsync(update.Message.Chat.Id, $"你好，@{member.Username}！您已申请加入本群组\n请点击下方按钮进行人机验证，本次验证将于三分钟后失效。", replyMarkup: new InlineKeyboardMarkup(new[]
+        {
+            InlineKeyboardButton.WithCallbackData("验证", "0"),
+            InlineKeyboardButton.WithCallbackData("人工通过", "1")
+        })).Result;
+        data[member.Id][update.Message.Chat.Id] = msg.MessageId;
+        Timer timer = new()
+        {
+            AutoReset = false,
+            Interval = 300000
+        };
+        timer.Elapsed += (_, _) =>
+        {
+            _ = botClient.DeleteMessageAsync(update.CallbackQuery.Message.Chat.Id, update.CallbackQuery.Message.MessageId);
+            _ = botClient.BanChatMemberAsync(update.Message.Chat.Id, member.Id);
+            _ = botClient.UnbanChatMemberAsync(update.Message.Chat.Id, member.Id);
+            _ = data[member.Id].Remove(update.Message.Chat.Id);
+        };
+        timer.Start();
     }
-    data[update.ChatJoinRequest.From.Id].Add(botClient.SendTextMessageAsync(update.ChatJoinRequest.From.Id, $"您正在加入{update.ChatJoinRequest.Chat.LastName}\n点击下方按钮确认您不是机器人", replyMarkup: new ReplyKeyboardMarkup(new[]
-    {
-        new KeyboardButton[] { "确认" },
-    })).Result.MessageId, update.ChatJoinRequest.Chat.Id);
 }, default);
+while (true)
+{
+    _ = Console.ReadLine();
+}
