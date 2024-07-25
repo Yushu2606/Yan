3614c639b9304694525b7684942b23bb3bbcd5ab
予纾 <100036558+Yuisyuu@users.noreply.github.com>
2024-07-25 18:32:57 +0800

    fix: remove `try...catch`

--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,6 @@ using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Yan.Utils;
-using File = System.IO.File;
 
 namespace Yan;
 
@@ -45,81 +44,67 @@ internal static class Program
     {
         BotClient.StartReceiving(async (_, update, _) =>
         {
-            try
+            switch (update.Type)
             {
-                switch (update.Type)
-                {
-                    case UpdateType.CallbackQuery:
+                case UpdateType.CallbackQuery:
+                    {
+                        if (update.CallbackQuery is null)
                         {
-                            if (update.CallbackQuery is null)
-                            {
-                                break;
-                            }
-
-                            await update.CallbackQuery.OnCallback();
                             break;
                         }
-                    case UpdateType.Message:
+
+                        await update.CallbackQuery.OnCallback();
+                        break;
+                    }
+                case UpdateType.Message:
+                    {
+                        if (update.Message is null)
                         {
-                            if (update.Message is null)
-                            {
-                                break;
-                            }
-
-                            switch (update.Message.Type)
-                            {
-                                case MessageType.Text:
-                                    {
-                                        if (update.Message.Text != $"/set@{(await BotClient.GetMeAsync()).Username}")
-                                        {
-                                            break;
-                                        }
-
-                                        await update.Message.OnSet();
-                                        break;
-                                    }
-                                case MessageType.ChatMembersAdded:
-                                    {
-                                        if (update.Message.NewChatMembers is null ||
-                                            !GroupData.TryGetValue(update.Message.Chat.Id,
-                                                out Dictionary<long, int>? data))
-                                        {
-                                            break;
-                                        }
-
-                                        foreach (User member in update.Message.NewChatMembers)
-                                        {
-                                            await member.OnJoin(update.Message.Chat.Id, data);
-                                        }
-
-                                        break;
-                                    }
-                            }
-
                             break;
                         }
-                    case UpdateType.ChatJoinRequest:
+
+                        switch (update.Message.Type)
                         {
-                            if (update.ChatJoinRequest is null || update.ChatJoinRequest.From.IsBot)
+                            case MessageType.Text:
                             {
+                                if (update.Message.Text != $"/set@{(await BotClient.GetMeAsync()).Username}")
+                                {
+                                    break;
+                                }
+
+                                await update.Message.OnSet();
                                 break;
                             }
+                            case MessageType.ChatMembersAdded:
+                            {
+                                if (update.Message.NewChatMembers is null ||
+                                    !GroupData.TryGetValue(update.Message.Chat.Id,
+                                        out Dictionary<long, int>? data))
+                                {
+                                    break;
+                                }
 
-                            await update.ChatJoinRequest.OnRequest();
+                                foreach (User member in update.Message.NewChatMembers)
+                                {
+                                    await member.OnJoin(update.Message.Chat.Id, data);
+                                }
+
+                                break;
+                            }
+                        }
+
+                        break;
+                    }
+                case UpdateType.ChatJoinRequest:
+                    {
+                        if (update.ChatJoinRequest is null || update.ChatJoinRequest.From.IsBot)
+                        {
                             break;
                         }
-                }
-            }
-            catch (Exception ex)
-            {
-                Directory.CreateDirectory("logs");
-                string logFilePath = $"logs/{DateTime.Now:yy-MM-ddTHH-mm-ss}.log";
-                if (File.Exists(logFilePath))
-                {
-                    await File.AppendAllTextAsync(logFilePath, "\n");
-                }
 
-                await File.AppendAllTextAsync(logFilePath, ex.ToString());
+                        await update.ChatJoinRequest.OnRequest();
+                        break;
+                    }
             }
         }, (_, e, _) => { });
         Thread.CurrentThread.Join();
